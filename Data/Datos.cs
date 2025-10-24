c24c7a8d09665e24bcc9a6aa88e7753c75171a04
CarlosRdzHdz23130629 <alu.23130629@correo.itlalaguna.edu.mx>
2025-10-24 14:21:00 -0600

    Agregar archivos de proyecto.

---
+++ Data/Datos.cs
@@ -0,0 +1,112 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMercadoProyecto.Data
+{
+    internal class Datos
+    {
+        private string connectionString =
+            "Host=localhost;Username=admin;Password=carlos2313;" +
+            "Database=supermercado";
+        private NpgsqlConnection GetConnection()
+        {
+            try
+            {
+                NpgsqlConnection connection =
+                    new NpgsqlConnection(connectionString);
+                connection.Open();
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al " +
+                    "conectar a la base de datos: " + ex.Message);
+                return null;
+            }
+        }
+
+        public DataSet getAllData(string command)
+        {
+            try
+            {
+                using (var connection = GetConnection())
+                {
+                    if (connection == null)
+                    {
+                        MessageBox.Show("No se pudo abrir la conexión con PostgreSQL");
+                        return null;
+                    }
+
+                    DataSet dataSet = new DataSet();
+                    NpgsqlDataAdapter da = new NpgsqlDataAdapter(command, connection);
+                    da.Fill(dataSet);
+
+                    return dataSet;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener datos: " + ex.Message);
+                return null;
+            }
+        }
+
+
+
+        public bool ExecuteQuery(string query)
+        {
+            using (var connection = GetConnection())
+            {
+                if (connection == null)
+                    return false;
+
+                try
+                {
+                    using (var comando = new NpgsqlCommand(query, connection))
+                    {
+                        comando.ExecuteNonQuery();
+                        return true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al ejecutar comando: " + ex.Message);
+                    return false;
+                }
+            }
+        }
+
+
+
+        public bool TestConnection()
+        {
+            try
+            {
+                using (var connection = GetConnection())
+                {
+                    if (connection != null && connection.State ==
+                        System.Data.ConnectionState.Open)
+                    {
+                        Console.WriteLine("Conexión exitosa a la base de datos.");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No se pudo establecer la conexión.");
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al probar la conexión: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ FormPrincipal.cs
@@ -0,0 +1,33 @@
+﻿using SuperMercadoProyecto.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperMercadoProyecto
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+       
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            Datos data = new Datos();
+            bool conexion = data.TestConnection();
+
+            if (conexion)
+                MessageBox.Show("Conectado");
+            else
+                MessageBox.Show("No Conectado");
+
+        }
+    }
+}
